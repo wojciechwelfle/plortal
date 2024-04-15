d4defcc742033e858eba2b0d5849284c4c11e3d7
Wafelek <111795716+wojciechwelfle@users.noreply.github.com>
2024-04-15 05:26:16 +0200

    W-003 - add User class constructors, getters and setters

--- backend/plortal/src/main/java/com/plortal/plortal/User.java
+++ backend/plortal/src/main/java/com/plortal/plortal/User.java
@@ -1,8 +1,21 @@
 package com.plortal.plortal;
 
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class User {
     private int id;
     private String email;
     private String password;
+
+    public User(int id, String email, String password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User() {
+    }
 }
