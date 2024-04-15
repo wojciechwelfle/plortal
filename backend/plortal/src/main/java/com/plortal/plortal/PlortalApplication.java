57b4fb5f1c744ca604df0edb7b5ed59b28829209
Wafelek <111795716+wojciechwelfle@users.noreply.github.com>
2024-04-15 04:44:52 +0200

    W-001 - generate backend project

---
+++ backend/plortal/src/main/java/com/plortal/plortal/PlortalApplication.java
@@ -0,0 +1,13 @@
+package com.plortal.plortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlortalApplication.class, args);
+	}
+
+}
---
+++ backend/plortal/src/test/java/com/plortal/plortal/PlortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plortal.plortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
