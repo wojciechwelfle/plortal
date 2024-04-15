19d6fdbcea09a0ee19e3459991339109621ee405
Wafelek <111795716+wojciechwelfle@users.noreply.github.com>
2024-04-15 05:57:27 +0200

    W-004 - add UserService class

---
+++ backend/plortal/src/main/java/com/plortal/plortal/UserService.java
@@ -0,0 +1,17 @@
+package com.plortal.plortal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+}
