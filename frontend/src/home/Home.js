aacd20338c06f01a1943dfa46d9d7d5c7e105a9f
Wafelek <111795716+wojciechwelfle@users.noreply.github.com>
2024-04-16 22:19:07 +0200

    W-011 - add logout button

--- frontend/src/home/Home.js
+++ frontend/src/home/Home.js
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
 import Card from 'react-bootstrap/Card';
-
-
+import { Button } from "react-bootstrap";
+import { logoutUser } from "../routes/userAuthorization";
 class Home extends Component {
     render() {
         return (
             <>
                <Card body>Home Page</Card>
+               <div className="d-grid gap-2 myDiv">
+                    <Button
+                        variant="dark"
+                        className="Btn"
+                        type="submit"
+                        onClick={logoutUser}
+                    >
+                        Logout
+                    </Button>
+                </div>
             </>
         );
     }
