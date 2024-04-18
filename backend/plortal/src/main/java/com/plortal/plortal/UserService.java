a578a8278f69b663d4ffceb923bb0e19caf209d3
Martyna Szymańska <martynaszymanska02@gmail.com>
2024-04-18 13:37:23 +0200

    W-009 - add isEmailValid to UserService

--- backend/plortal/src/main/java/com/plortal/plortal/UserService.java
+++ backend/plortal/src/main/java/com/plortal/plortal/UserService.java
@@ -21,6 +21,9 @@ public class UserService {
 
     public void addNewUser(User user) {
         Optional<User> userOptional = userRepository.findUserByEmail(user.getEmail());
+        if(!isEmailValid(user.getEmail())){
+            throw new IllegalStateException("Email " + user.getEmail() + " does not meet the requirements");
+        }
         if (userOptional.isPresent()) {
             throw new IllegalStateException("Email " + user.getEmail() + " is taken");
         }
@@ -36,4 +39,9 @@ public class UserService {
         return userOptional.isPresent() && userOptional.get().getPassword().equals(user.getPassword());
     }
 
+    private boolean isEmailValid(String email){
+        return EmailValidator.validateEmail(email);
+    }
+
 }
+
