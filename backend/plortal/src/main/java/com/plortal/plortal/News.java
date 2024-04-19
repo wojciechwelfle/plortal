dfe06bf2b437d5493c903c2d2dc5dae6dfc48a25
Wafelek <111795716+wojciechwelfle@users.noreply.github.com>
2024-04-19 23:35:23 +0200

    W-017 - modeling News class in database

--- backend/plortal/src/main/java/com/plortal/plortal/News.java
+++ backend/plortal/src/main/java/com/plortal/plortal/News.java
@@ -1,19 +1,32 @@
 package com.plortal.plortal;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.NonNull;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "News")
 public class News {
     @Id
     int id;
 
+    @NonNull
+    @Column(name = "title", length = 70)
     private String title;
 
+    @NonNull
+    @Column(name = "photoUrl", length = 150)
     private String photoUrl;
 
+    @NonNull
+    @Column(name = "description", length = 300)
     private String description;
 
+    @NonNull
+    @Column(name = "modificationDate")
     private LocalDateTime modificationDate;
 }
