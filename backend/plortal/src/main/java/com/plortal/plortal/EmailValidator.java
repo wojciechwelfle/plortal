f19cb85141712200affca09edbe9f774dc07344d
Martyna Szymańska <martynaszymanska02@gmail.com>
2024-04-20 00:49:30 +0200

    W-009 - add email max length in EmailValidator

--- backend/plortal/src/main/java/com/plortal/plortal/EmailValidator.java
+++ backend/plortal/src/main/java/com/plortal/plortal/EmailValidator.java
@@ -4,6 +4,6 @@ public class EmailValidator {
 
      public static boolean validateEmail(String email) {
         String emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
-        return email.matches(emailRegex) && email.length() >= 7;
+        return email.matches(emailRegex) && email.length() >= 7 && email.length() <= 100 ;
     }
 }
