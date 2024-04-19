83676eea33cd5f823a87a85fdc550bcbdae89992
Wafelek <111795716+wojciechwelfle@users.noreply.github.com>
2024-04-19 23:37:23 +0200

    W-017 - add getters, setters and constructors

--- backend/plortal/src/main/java/com/plortal/plortal/News.java
+++ backend/plortal/src/main/java/com/plortal/plortal/News.java
@@ -4,12 +4,16 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.NonNull;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "News")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class News {
     @Id
     int id;
