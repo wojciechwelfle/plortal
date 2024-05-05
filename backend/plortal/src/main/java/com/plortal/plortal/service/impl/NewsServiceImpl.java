4902f7c6236ca3436520250d42d3aa1957b50314
Wafelek <111795716+wojciechwelfle@users.noreply.github.com>
2024-05-05 16:44:28 +0200

    W-032 - add NewsService Implementation

--- backend/plortal/src/main/java/com/plortal/plortal/service/impl/NewsServiceImpl.java
+++ backend/plortal/src/main/java/com/plortal/plortal/service/impl/NewsServiceImpl.java
@@ -1,9 +1,10 @@
-package com.plortal.plortal.service;
+package com.plortal.plortal.service.impl;
 
 import com.plortal.plortal.model.News;
 import com.plortal.plortal.dto.NewsDTO;
 import com.plortal.plortal.mapper.NewsDTOMapper;
 import com.plortal.plortal.repository.NewsRepository;
+import com.plortal.plortal.service.NewsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,20 +13,22 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-public class NewsService {
+public class NewsServiceImpl implements NewsService {
     private final NewsRepository newsRepository;
     private final NewsDTOMapper newsDtoMapper;
 
     @Autowired
-    public NewsService(NewsRepository newsRepository, NewsDTOMapper newsDtoMapper) {
+    public NewsServiceImpl(NewsRepository newsRepository, NewsDTOMapper newsDtoMapper) {
         this.newsRepository = newsRepository;
         this.newsDtoMapper = newsDtoMapper;
     }
 
+    @Override
     public void addNews(NewsDTO newsDto) {
         newsRepository.save(newsDtoMapper.fromDTO(newsDto));
     }
 
+    @Override
     public List<NewsDTO> getAllNews() {
         return newsRepository.findAll()
                 .stream()
