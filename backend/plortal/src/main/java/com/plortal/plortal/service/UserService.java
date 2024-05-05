2084d4457eac74513b2aeb8b2e464ea499d72b5b
Wafelek <111795716+wojciechwelfle@users.noreply.github.com>
2024-05-05 16:45:45 +0200

    W-032 - add UserService Interface

---
+++ backend/plortal/src/main/java/com/plortal/plortal/service/UserService.java
@@ -0,0 +1,11 @@
+package com.plortal.plortal.service;
+
+import com.plortal.plortal.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    List<User> getUsers();
+    void addNewUser(User user);
+    void authenticateUser(String email, String password);
+}
