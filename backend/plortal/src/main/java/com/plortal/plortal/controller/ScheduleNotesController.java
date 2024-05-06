58dc3e9a58295224ac3a43ad4c7c60cdec2e120e
Martyna Szymańska <martynaszymanska02@gmail.com>
2024-05-07 00:21:24 +0200

    W-026 - fix bug in ScheduleNotesController

--- backend/plortal/src/main/java/com/plortal/plortal/controller/ScheduleNotesController.java
+++ backend/plortal/src/main/java/com/plortal/plortal/controller/ScheduleNotesController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 import java.util.List;
 
 @RestController
@@ -33,7 +34,7 @@ public class ScheduleNotesController {
         }
     }
 
-    @GetMapping("/api/v1/schedulenotes/all")
+    @GetMapping("/api/v1/schedulenotes")
     public ResponseEntity<List<ScheduleNotes>> getAllScheduleNotes() {
         List<ScheduleNotes> notes = service.findAll();
         if (notes.isEmpty()) {
@@ -42,14 +43,18 @@ public class ScheduleNotesController {
         return new ResponseEntity<>(notes, HttpStatus.OK);
     }
 
-    @GetMapping("/api/v1/schedulenotes/all")
+    @GetMapping("/api/v1/schedulenotes/date")
     public ResponseEntity<List<ScheduleNotes>> getNotesByDate(@RequestParam String date) {
         LocalDate parsedDate = LocalDate.parse(date);
         String dateString = parsedDate.toString();
-        List<ScheduleNotes> notes = service.findByDate(dateString);
-        if (notes.isEmpty()) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        try {
+            List<ScheduleNotes> notes = service.findByDate(dateString);
+            if (notes.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+                return new ResponseEntity<>(notes, HttpStatus.OK);
+        } catch(DateTimeParseException e) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
         }
-        return new ResponseEntity<>(notes, HttpStatus.OK);
     }
 }
