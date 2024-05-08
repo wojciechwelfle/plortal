e2d98b5429c31c0477c91942373f573268c5dc08
Martyna Szymańska <martynaszymanska02@gmail.com>
2024-05-08 15:13:25 +0200

    W-040 - replace homePage by NewsPage

--- frontend/src/routes/PageRouter.jsx
+++ frontend/src/routes/PageRouter.jsx
@@ -4,7 +4,6 @@ import ProtectedRoute from "./ProtectedRoute";
 
 import LoginPage from "../pages/LoginPage";
 import RegisterPage from "../pages/RegisterPage";
-import HomePage from "../pages/HomePage";
 import SchedulePage from "../pages/SchedulePage";
 import NewsPage from "../pages/NewsPage";
 import Facility from "../pages/FacilityPage";
@@ -18,14 +17,6 @@ const PageRouter = () => {
                 <Routes>
                     <Route exact path="/" element={<RegisterPage />} />
                     <Route exact path="/login" element={<LoginPage />} />
-
-                    <Route
-                        exact
-                        path="/home"
-                        element={
-                            <ProtectedRoute path="/home" element={HomePage} />
-                        }
-                    />
                     <Route
                         exact
                         path="/schedule"
