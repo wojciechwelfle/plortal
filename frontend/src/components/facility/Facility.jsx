35590ea44728860b5d2e2c767ac504fe814fdf98
Martyna Szymańska <martynaszymanska02@gmail.com>
2024-05-14 01:10:32 +0200

    W-039 - added third theme to Facility.jsc

--- frontend/src/components/facility/Facility.jsx
+++ frontend/src/components/facility/Facility.jsx
@@ -1,50 +1,48 @@
 import { useEffect, useState } from 'react';
 import "./Facility.css";
 import FontSizeBar from "./FontSizeBar";
-import ThemeSwitch from "./ThemeSwitch";
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
 import "../././../GlobalColors.css";
 
 const Facility = () => {
     const savedFontSize = parseInt(localStorage.getItem('fontSize'), 10) || 20;
     const savedTheme = localStorage.getItem('theme') || 'light';
     const [fontSize, setFontSize] = useState(savedFontSize);
-    const [isDarkTheme, setIsDarkTheme] = useState(savedTheme === 'dark');
+    const [theme, setTheme] = useState(savedTheme);
+    const [tempTheme, setTempTheme] = useState(savedTheme);
     const [tempFontSize, setTempFontSize] = useState(savedFontSize);
-    const [tempTheme, setTempTheme] = useState(savedTheme === 'dark');
 
     const handleFontSizeChange = (event) => {
         const newSize = parseInt(event.target.value, 10);
         setTempFontSize(newSize);
     };
 
-    const handleThemeChange = () => {
-        setTempTheme(!tempTheme);
+    const handleThemeChange = (event) => {
+        setTempTheme(event.target.value);
     };
 
     const handleApplyChanges = () => {
         setFontSize(tempFontSize);
-        setIsDarkTheme(tempTheme);
+        setTheme(tempTheme);
         localStorage.setItem('fontSize', tempFontSize);
-        localStorage.setItem('theme', tempTheme ? 'dark' : 'light');
+        localStorage.setItem('theme', tempTheme);
         document.documentElement.style.setProperty('--font-size', `${tempFontSize}px`);
-        document.documentElement.classList.add(tempTheme ? 'dark-theme' : 'light-theme');
-        document.documentElement.classList.remove(tempTheme ? 'light-theme' : 'dark-theme');
+        document.documentElement.classList.remove('light-theme', 'dark-theme', 'blue-theme');
+        document.documentElement.classList.add(`${tempTheme}-theme`);
     };
 
     useEffect(() => {
         document.documentElement.style.setProperty('--font-size', `${fontSize}px`);
-        if (isDarkTheme) {
-            document.documentElement.classList.add('dark-theme');
-            document.documentElement.classList.remove('light-theme');
-        } else {
-            document.documentElement.classList.add('light-theme');
-            document.documentElement.classList.remove('dark-theme');
-        }
-    }, [fontSize, isDarkTheme]);
+        document.documentElement.classList.remove('light-theme', 'dark-theme', 'blue-theme');
+        document.documentElement.classList.add(`${theme}-theme`);
+    }, [fontSize, theme]);
 
     const tempThemeStyles = {
-        backgroundColor: tempTheme ? 'var(--dark-background)' : 'var(--light-background)',
-        color: tempTheme ? 'var(--dark-text)' : 'var(--light-text)',
+        backgroundColor: `var(--${tempTheme}-background)`,
+        color: `var(--${tempTheme}-text)`,
     };
 
     return (
@@ -62,9 +60,21 @@ const Facility = () => {
                             </div>
                         </div>
                         <div className="control-group">
-                            <label>Kontrast</label>
+                            <label>Motyw</label>
                             <div className="buttons">
-                                <ThemeSwitch onChange={handleThemeChange} checked={tempTheme} />
+                                <FormControl variant="outlined">
+                                    <InputLabel id="theme-select-label" className="theme-select">Kontrast</InputLabel>
+                                    <Select
+                                        labelId="theme-select-label"
+                                        value={tempTheme}
+                                        onChange={handleThemeChange}
+                                        label="Motyw"
+                                    >
+                                        <MenuItem value="light">Jasny</MenuItem>
+                                        <MenuItem value="dark">Ciemny</MenuItem>
+                                        <MenuItem value="blue">Niebieski</MenuItem>
+                                    </Select>
+                                </FormControl>
                             </div>
                         </div>
                     </div>
