3ccfcdf9ed149bf4eb2a61c3ae3a9734059ef3a7
Wafelek <111795716+wojciechwelfle@users.noreply.github.com>
2024-05-15 00:51:15 +0200

    W-056 - role based authorization

--- frontend/src/routes/ProtectedRoute.jsx
+++ frontend/src/routes/ProtectedRoute.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
-import { isUserLogin } from "./userAuthorization";
+import { isUserLogin, userRole } from "./userAuthorization";
 
-const ProtectedRoute = ({ element: Element, ...rest }) => {
-    if (isUserLogin()) {
+const ProtectedRoute = ({ element: Element, requiredRoles, ...rest }) => {
+    const role = userRole();
+    console.log(role);
+
+    if (isUserLogin() && requiredRoles.includes(role)) {
         return <Element {...rest} />;
     } else {
         return <Navigate to="/login" />;
