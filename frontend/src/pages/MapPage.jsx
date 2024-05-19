e2f7f02653a0347ceb585609102bbaa0b192e4db
WOJTASK1 <wojtek03.lep@gmail.com>
2024-05-19 13:53:28 +0200

    W-012_3 self-moving map

--- frontend/src/pages/MapPage.jsx
+++ frontend/src/pages/MapPage.jsx
@@ -2,15 +2,16 @@ import React, { useState } from "react";
 import MapComponent from "../components/map/MapComponent";
 import NavigationBar from "../components/navbar/Navbar";
 
+
 const MapPage = () => {
-    const [showOffcanvas, setShowOffcanvas] = useState(true);
+    const [showOffcanvas, setShowOffcanvas] = useState(false);
 
     return (
         <>
             <NavigationBar showOffcanvas={showOffcanvas} setShowOffcanvas={setShowOffcanvas} />
             
-                <MapComponent style={{ marginLeft: showOffcanvas ? '5em' : '' }}/>
-           
+            {/*<MapComponent style={{ marginLeft: showOffcanvas ? '5em' : '0' }}/> */}
+            <MapComponent movedToTheLeft="setShowOffcanvas(showOffcanvas)"/>
         </>
     );
 };
