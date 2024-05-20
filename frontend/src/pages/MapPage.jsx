7a4d4fed0490ffa3c022c413fc136e84d737db0e
WOJTASK1 <wojtek03.lep@gmail.com>
2024-05-20 12:15:08 +0200

    W-012_3 refactoring

--- frontend/src/pages/MapPage.jsx
+++ frontend/src/pages/MapPage.jsx
@@ -3,14 +3,10 @@ import MapComponent from "../components/map/MapComponent";
 import NavigationBar from "../components/navbar/Navbar";
 
 const MapPage = () => {
-  const [showOffcanvas, setShowOffcanvas] = useState(true);
   return (
     <>
-      <NavigationBar
-        showOffcanvas={showOffcanvas}
-        setShowOffcanvas={setShowOffcanvas}
-      />
-      <MapComponent movedToTheLeft={showOffcanvas} />
+      <NavigationBar />
+      <MapComponent />
     </>
   );
 };
