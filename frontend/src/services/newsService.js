0d20238b470c6d348b3973ea0922e90e3dad26c1
Wafelek <111795716+wojciechwelfle@users.noreply.github.com>
2024-05-21 00:52:00 +0200

    W-057 - add deleteNews service method

--- frontend/src/services/newsService.js
+++ frontend/src/services/newsService.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
-const REST_API_URL = 'http://localhost:8080/api/v1/news';
+const REST_API_URL = "http://localhost:8080/api/v1/news";
 
 export const getAllNews = () => axios.get(REST_API_URL);
+
+export const deleteNews = (id, loginRequest) => axios.delete(`${REST_API_URL}/${id}`, { data: loginRequest });
