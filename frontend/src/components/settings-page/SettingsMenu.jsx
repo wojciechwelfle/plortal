c05b213cedcb6c1552e7cec219bd8855ce5a9000
Martyna Szymańska <martynaszymanska02@gmail.com>
2024-05-20 16:51:35 +0200

    W-046 - added navbarSettingsMenu

--- frontend/src/components/settings-page/SettingsMenu.jsx
+++ frontend/src/components/settings-page/SettingsMenu.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import LogoutButton from "../LogoutButton";
+import NavbarSettingsMenu from "./NavbarSettingsMenu";
+import "./SettingsMenu.css";
 
 const SettingsMenu = () => {
     return (
         <>
-            <Card body>
-                Ustawienia <LogoutButton />{" "}
+            <Card>
+                <LogoutButton />
             </Card>
+            <h2>Ustawienia</h2>
+            <NavbarSettingsMenu />
+            <div className="menu">
+                    <p>Przykład strony</p>
+            </div>
         </>
     );
 };
