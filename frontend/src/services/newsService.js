90b3c9752ff7f4744c3a36da1bcd424973e799ec
Wafelek <111795716+wojciechwelfle@users.noreply.github.com>
2024-05-21 23:39:18 +0200

    W-057 - addNews service function

--- frontend/src/services/newsService.js
+++ frontend/src/services/newsService.js
@@ -5,3 +5,5 @@ const REST_API_URL = "http://localhost:8080/api/v1/news";
 export const getAllNews = () => axios.get(REST_API_URL);
 
 export const deleteNews = (id, loginRequest) => axios.delete(`${REST_API_URL}/${id}`, { data: loginRequest });
+
+export const addNews = (news) => axios.post(REST_API_URL, news);
