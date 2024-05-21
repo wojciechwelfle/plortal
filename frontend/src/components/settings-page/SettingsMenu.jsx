549d602b5b28846038a12e747c54fb8d58b46914
Martyna Szymańska <martynaszymanska02@gmail.com>
2024-05-21 11:21:19 +0200

    W-062 changed css to SettingsMenu

--- frontend/src/components/settings-page/SettingsMenu.jsx
+++ frontend/src/components/settings-page/SettingsMenu.jsx
@@ -1,18 +1,21 @@
-import React from "react";
-import Card from "react-bootstrap/Card";
-import LogoutButton from "../LogoutButton";
-import NavbarSettingsMenu from "./NavbarSettingsMenu";
-import "./SettingsMenu.css";
-import NavigationBar from "../navbar/Navbar";
+import React, { useState } from 'react';
+import NavbarSettingsMenu from './NavbarSettingsMenu';
+import ChangePassword from './ChangePassword';
+import DeleteNote from './DeleteNote';
+import './SettingsMenu.css';
+import NavigationBar from '../navbar/Navbar';
 
 const SettingsMenu = () => {
+    const [activeTab, setActiveTab] = useState('');
+
     return (
         <>
             <NavigationBar />
             <h2>Ustawienia</h2>
-            <NavbarSettingsMenu />
+            <NavbarSettingsMenu setActiveTab={setActiveTab} />
             <div className="menu">
-                    <p>Przykład strony</p>
+                {activeTab === 'change-password' && <ChangePassword />}
+                {activeTab === 'delete-note' && <DeleteNote />}
             </div>
         </>
     );
