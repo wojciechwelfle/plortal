8080f9c45ac2bb4ffd591ee53b4940fe3f6b084e
Martyna Szymańska <martynaszymanska02@gmail.com>
2024-05-21 11:22:19 +0200

    W-062 changed NavbarSettingMenu ActiveTab

--- frontend/src/components/settings-page/NavbarSettingsMenu.jsx
+++ frontend/src/components/settings-page/NavbarSettingsMenu.jsx
@@ -3,12 +3,12 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import './SettingsMenu.css';
 
-const NavbarSettingsMenu = () => {
+const NavbarSettingsMenu = ({ setActiveTab }) => {
     return (
         <Navbar className="custom-navbar">
             <Nav className="me-auto navbar-menu">
-                <Nav.Link href="#change-password" className="navbar-button">Zmień hasło</Nav.Link>
-                <Nav.Link href="#delete-note" className="navbar-button">Usuń notatkę</Nav.Link>
+                <Nav.Link onClick={() => setActiveTab('change-password')} className="navbar-button">Zmień hasło</Nav.Link>
+                <Nav.Link onClick={() => setActiveTab('delete-note')} className="navbar-button">Usuń notatkę</Nav.Link>
             </Nav>
         </Navbar>
     );
