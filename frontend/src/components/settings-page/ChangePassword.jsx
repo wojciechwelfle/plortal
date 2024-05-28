b05c804ea49c21783742f7804d04ddc8e92709eb
Martyna Szymańska <martynaszymanska02@gmail.com>
2024-05-28 22:01:44 +0200

    W-047 refactor ChangePassword.jsx code

--- frontend/src/components/settings-page/ChangePassword.jsx
+++ frontend/src/components/settings-page/ChangePassword.jsx
@@ -5,63 +5,106 @@ import PasswordStrength from "../register-login/PasswordStrength";
 import { changePasswordByUserId } from "../../services/userService";
 
 const ChangePassword = () => {
-    const [newPasswordStrength, setNewPasswordStrength] = useState("");
-    const [confirmPasswordStrength, setConfirmPasswordStrength] = useState("");
-    const [actualPasswordError, setActualPasswordError] = useState(false);
-    const [newPasswordError, setNewPasswordError] = useState(false);
-    const [confirmPasswordError, setConfirmPasswordError] = useState(false);
-    const [changePasswordMessage, setChangePasswordMessage] = useState(false);
-    const [changePasswordMessageWeak, setChangePasswordMessageWeak] = useState(false);
-    const [actualPassword, setActualPassword] = useState("");
-    const [newPassword, setNewPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
+    const [passwordStrength, setPasswordStrength] = useState({
+        newPassword: "",
+        confirmPassword: ""
+    });
+    const [errors, setErrors] = useState({
+        actualPassword: false,
+        newPassword: false,
+        confirmPassword: false
+    });
+    const [changePasswordMessage, setChangePasswordMessage] = useState("");
+    const [passwords, setPasswords] = useState({
+        actualPassword: "",
+        newPassword: "",
+        confirmPassword: ""
+    });
     const ActualPassword = localStorage.getItem("password");
 
     const handleChangePassword = (event) => {
         event.preventDefault();
         const userId = localStorage.getItem("id");
 
-        if (newPasswordStrength === "Weak") {
-            setChangePasswordMessage(false);
-            setChangePasswordMessageWeak(true);
+        if (passwordStrength.newPassword === "Weak") {
+            setChangePasswordMessage("New password is weak");
             return;
         }
 
-        if( newPassword === ActualPassword) {
-            setChangePasswordMessage(false);
-            setChangePasswordMessageWeak(false);
+        if (passwords.newPassword === ActualPassword) {
+            setChangePasswordMessage("New password cannot be the same as the old password");
             return;
         }
 
-    if (newPassword === confirmPassword && actualPassword === ActualPassword && actualPassword !== newPassword) {
-            changePasswordByUserId(userId, newPassword)
+        if (passwords.newPassword === passwords.confirmPassword && passwords.actualPassword === ActualPassword && passwords.actualPassword !== passwords.newPassword) {
+            changePasswordByUserId(userId, passwords.actualPassword, passwords.newPassword)
                 .then((response) => {
                     console.log(response.status);
-                    localStorage.setItem("password", newPassword);
-                    setChangePasswordMessage(true);
-                    setChangePasswordMessageWeak(false);
+                    localStorage.setItem("password", passwords.newPassword);
+                    setChangePasswordMessage(response.data);
                     resetFields();
                 })
                 .catch((error) => {
                     console.error(error);
+                    setChangePasswordMessage(error.response.data);
                 });
+        } else {
+            if (passwords.newPassword !== passwords.confirmPassword) {
+                setChangePasswordMessage("New password and confirmation password do not match");
+            } else if (passwords.actualPassword !== ActualPassword) {
+                setChangePasswordMessage("Current password is incorrect");
+            }
         }
     };
 
     const resetFields = () => {
-        setActualPassword("");
-        setNewPassword("");
-        setConfirmPassword("");
+        setPasswords({
+            actualPassword: "",
+            newPassword: "",
+            confirmPassword: ""
+        });
     };
 
-    const handleActualPassword = (event) => {
-        const password = event.target.value;
-        setActualPassword(password);
+    const handlePasswordChange = (event) => {
+        const { name, value } = event.target;
+        setPasswords((prevState) => ({
+            ...prevState,
+            [name]: value
+        }));
 
-        if (password !== ActualPassword) {
-            setActualPasswordError(true);
-        } else {
-            setActualPasswordError(false);
+        if (name === "actualPassword") {
+            setErrors((prevState) => ({
+                ...prevState,
+                actualPassword: value !== ActualPassword
+            }));
+        }
+
+        if (name === "newPassword" || name === "confirmPassword") {
+            const strength = evaluatePasswordStrength(value);
+            setPasswordStrength((prevState) => ({
+                ...prevState,
+                [name]: strength
+            }));
+
+            if (name === "newPassword" && value !== passwords.confirmPassword) {
+                setErrors((prevState) => ({
+                    ...prevState,
+                    newPassword: true,
+                    confirmPassword: true
+                }));
+            } else if (name === "confirmPassword" && value !== passwords.newPassword) {
+                setErrors((prevState) => ({
+                    ...prevState,
+                    newPassword: true,
+                    confirmPassword: true
+                }));
+            } else {
+                setErrors((prevState) => ({
+                    ...prevState,
+                    newPassword: false,
+                    confirmPassword: false
+                }));
+            }
         }
     };
 
@@ -93,36 +136,6 @@ const ChangePassword = () => {
         }
     };
 
-    const handleNewPasswordChange = (event) => {
-        const password = event.target.value;
-        setNewPassword(password);
-        const strength = evaluatePasswordStrength(password);
-        setNewPasswordStrength(strength);
-
-        if (password !== confirmPassword) {
-            setNewPasswordError(true);
-            setConfirmPasswordError(true);
-        } else {
-            setNewPasswordError(false);
-            setConfirmPasswordError(false);
-        }
-    };
-
-    const handleConfirmPasswordChange = (event) => {
-        const password = event.target.value;
-        setConfirmPassword(password);
-        const strength = evaluatePasswordStrength(password);
-        setConfirmPasswordStrength(strength);
-
-        if (password !== newPassword) {
-            setConfirmPasswordError(true);
-            setNewPasswordError(true);
-        } else {
-            setConfirmPasswordError(false);
-            setNewPasswordError(false);
-        }
-    };
-
     return (
         <div className="change-password-container">
             <h3>Zmień hasło</h3>
@@ -130,29 +143,43 @@ const ChangePassword = () => {
                 <div>
                     <label>Aktualne hasło:</label>
                     <Form.Group className="group" controlId="actualPassword" size="lg">
-                        <Form.Control type="password" value={actualPassword} onChange={handleActualPassword} />
-                        {actualPasswordError && <p style={{ fontSize: "15px", color : 'red' }}>Current password is different</p>}
+                        <Form.Control
+                            type="password"
+                            name="actualPassword"
+                            value={passwords.actualPassword}
+                            onChange={handlePasswordChange}
+                        />
+                        {errors.actualPassword && <p style={{ fontSize: "15px", color : 'red' }}>Current password is different</p>}
                     </Form.Group>
                 </div>
                 <div>
                     <label>Nowe hasło:</label>
                     <Form.Group className="group" controlId="newPassword" size="lg">
-                        <Form.Control type="password" value={newPassword} onChange={handleNewPasswordChange} />
-                        <PasswordStrength strength={newPasswordStrength} />
-                        {newPasswordError && <p style={{ fontSize: "15px", color : 'red' }}>Passwords do not match</p>}
+                        <Form.Control
+                            type="password"
+                            name="newPassword"
+                            value={passwords.newPassword}
+                            onChange={handlePasswordChange}
+                        />
+                        <PasswordStrength strength={passwordStrength.newPassword} />
+                        {errors.newPassword && <p style={{ fontSize: "15px", color : 'red' }}>Passwords do not match</p>}
                     </Form.Group>
                 </div>
                 <div>
                     <label>Potwierdź nowe hasło:</label>
                     <Form.Group className="group" controlId="confirmPassword" size="lg">
-                        <Form.Control type="password" value={confirmPassword} onChange={handleConfirmPasswordChange} />
-                        <PasswordStrength strength={confirmPasswordStrength} />
-                        {confirmPasswordError && <p style={{ fontSize: "15px", color : 'red' }}>Passwords do not match</p>}
+                        <Form.Control
+                            type="password"
+                            name="confirmPassword"
+                            value={passwords.confirmPassword}
+                            onChange={handlePasswordChange}
+                        />
+                        <PasswordStrength strength={passwordStrength.confirmPassword} />
+                        {errors.confirmPassword && <p style={{ fontSize: "15px", color : 'red' }}>Passwords do not match</p>}
                     </Form.Group>
                 </div>
                 <button type="submit">Zmień hasło</button>
-                {changePasswordMessage && <p style={{ fontSize: "15px", color : 'green' }}>Password changed</p>}
-                {changePasswordMessageWeak && <p style={{ fontSize: "25px", color : 'red' }}>Password is too weak</p>}
+                {changePasswordMessage && <p style={{ fontSize: "15px", color : 'red' }}>{changePasswordMessage}</p>}
             </form>
         </div>
     );
