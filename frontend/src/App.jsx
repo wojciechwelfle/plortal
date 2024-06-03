4ad0c31b3f94414b826e6b15e24562d45c0a4d54
Martyna Szymańska <martynaszymanska02@gmail.com>
2024-06-03 20:50:30 +0200

    W-038 changed folder for ThemeContext

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import "./App.css";
 import PageRouter from "./routes/PageRouter";
+import { ThemeProvider } from './components/theme-context/ThemeContext';
 
 const App = () => {
     return (
         <>
+            <ThemeProvider>
+                <div>
             <PageRouter />
+                </div>
+            </ThemeProvider>
         </>
     );
 };
