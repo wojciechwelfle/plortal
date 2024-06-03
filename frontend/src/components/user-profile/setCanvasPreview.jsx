b2ece9e168421f7a6df4442deac713394c8d6348
szymon-muszynski <s.muszynski2003@gmail.com>
2024-06-03 16:06:54 +0200

    W-064 - delete comments from setCanvasPreview.jsx

--- frontend/src/components/user-profile/setCanvasPreview.jsx
+++ frontend/src/components/user-profile/setCanvasPreview.jsx
@@ -1,17 +1,13 @@
 const setCanvasPreview = (
-    image, // HTMLImageElement
-    canvas, // HTMLCanvasElement
-    crop // PixelCrop
+    image, 
+    canvas, 
+    crop 
   ) => {
     const ctx = canvas.getContext("2d");
     if (!ctx) {
       throw new Error("No 2d context");
     }
   
-    // devicePixelRatio slightly increases sharpness on retina devices
-    // at the expense of slightly slower render times and needing to
-    // size the image back down if you want to download/upload and be
-    // true to the images natural size.
     const pixelRatio = window.devicePixelRatio;
     const scaleX = image.naturalWidth / image.width;
     const scaleY = image.naturalHeight / image.height;
@@ -26,7 +22,6 @@ const setCanvasPreview = (
     const cropX = crop.x * scaleX;
     const cropY = crop.y * scaleY;
   
-    // Move the crop origin to the canvas origin (0,0)
     ctx.translate(-cropX, -cropY);
     ctx.drawImage(
       image,
