987ff6fb7c5d475ae0872ce96c97054c774a020f
szymon-muszynski <s.muszynski2003@gmail.com>
2024-06-05 12:57:13 +0200

    W-065 - update userService.js by adding frontend functions to get data from the database

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -18,3 +18,23 @@ export const changePasswordByUserId = (userId, oldPassword, newPassword) => {
         }
     });
 };
+
+export const updateBasicInfo = (userId, basicInfo) => {
+    return axios.post(`${REST_API_URL}/update-basic-info/${userId}`, null, {
+        params: {
+            basicInfo: basicInfo
+        }
+    });
+};
+
+export const updateAdditionalInfo = (userId, additionalInfo) => {
+    return axios.post(`${REST_API_URL}/update-additional-info/${userId}`, null, {
+        params: {
+            additionalInfo: additionalInfo
+        }
+    });
+};
+
+export const getUserById = (userId) => {
+    return axios.get(`${REST_API_URL}/${userId}`);
+};
