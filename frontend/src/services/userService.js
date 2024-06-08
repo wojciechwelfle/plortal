06986f24780bc722f60abcc9a4252e3675e10622
szymon-muszynski <s.muszynski2003@gmail.com>
2024-06-08 16:27:40 +0200

    W-065 - delete redundant functions from userService.js

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -18,27 +18,3 @@ export const changePasswordByUserId = (userId, oldPassword, newPassword) => {
         }
     });
 };
-
-export const updateBasicInfo = (userId, basicInfo) => {
-    return axios.post(`${REST_API_URL}/update-basic-info/${userId}`, null, {
-        params: {
-            basicInfo: basicInfo
-        }
-    });
-};
-
-export const updateAdditionalInfo = (userId, additionalInfo) => {
-    return axios.post(`${REST_API_URL}/update-additional-info/${userId}`, null, {
-        params: {
-            additionalInfo: additionalInfo
-        }
-    });
-};
-
-export const getBasicInfo = (userId) => {
-    return axios.get(`${REST_API_URL}/${userId}/basic-info`);
-};
-
-export const getAdditionalInfo = (userId) => {
-    return axios.get(`${REST_API_URL}/${userId}/additional-info`);
-};
